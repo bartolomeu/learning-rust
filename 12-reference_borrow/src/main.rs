1d5a10881eb40595f46cfd2c165c60d0dc92fa3d
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2023-02-26 21:49:08 -0300

    review e new sub chapter

--- 12-reference_borrow/src/main.rs
+++ 12-reference_borrow/src/main.rs
@@ -19,14 +19,14 @@ fn exercise01 () {
 fn exercise02(){
     let mut str = String::from("hi"); //var str is Mutable
 
-    // this line will make the next line break, cann't use a mutable refence and then any reference & to this value
-    //let something = &mut str; 
+    // this line will make the next line break, can't use a mutable refence and then any reference & to this value
+    // let something = &mut str; 
 
     change_str(&mut str); //pass str as reference, so I can reuse it at next line and set it as mutable to fnc change_str can change it...
     println!("str => {str}");
     
     //use this var after another refence to the same value breaks the code, but if we use it before que compiler will clean its reference and won't break
-    //println!("something => {something}");
+    // println!("something => {something}");
 
 
     fn change_str (str1: &mut String){
---
+++ 13-slice/src/main.rs
@@ -0,0 +1,6 @@
+//https://doc.rust-lang.org/book/ch04-03-slices.html
+
+
+fn main() {
+    println!("Hello, world!");
+}
